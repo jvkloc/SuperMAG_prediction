cc0c146cfc9727e8aa139237b52e1bda794e0576
jvkloc <120744120+jvkloc@users.noreply.github.com>
2025-04-06 21:17:04 +0300

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -1,37 +1,11 @@
-"""Functions for evaluating model predictions."""
+"""Functions for evaluating the model."""
 
-from matplotlib.figure import Figure
-from matplotlib.pyplot import show, subplots, tight_layout
-from numpy import mean as npmean, ndarray, std as npstd
+from numpy import mean as npmean, std as npstd
+from pandas import DataFrame
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 from xgboost import Booster
 
 from constants import TARGETS
-
-
-def plot_prediction(
-    y_pred: ndarray,
-    y_test: ndarray,
-    targets: list[str] = TARGETS
-) -> None:
-    """Plots the prediction vs. true values."""
-    _: ndarray; fig: Figure
-    _, fig = subplots(2, 2, figsize=(10, 10))
-    axes: ndarray = fig.flatten()
-    for i, (target, ax) in enumerate(zip(targets, axes)):
-        ax.scatter(y_test[:, i], y_pred[:, i], alpha=0.5)
-        ax.plot(
-            [y_test[:, i].min(), y_test[:, i].max()], 
-            [y_test[:, i].min(), y_test[:, i].max()], 
-            "r--",
-        )
-        ax.set_xlim(y_test[:, i].min() - 5, y_test[:, i].max() + 5)
-        ax.set_ylim(y_pred[:, i].min() - 5, y_pred[:, i].max() + 5)
-        ax.set_xlabel(f"Actual {target}")
-        ax.set_ylabel(f"Predicted {target}")
-        ax.set_title(f"Actual vs Predicted {target}")
-    tight_layout()
-    show()
 
 
 def print_average_metrics(
@@ -39,9 +13,9 @@ def print_average_metrics(
 ) -> None:
     """Prints the average metrics across all training folds."""
     print("\nAverage metrics across all folds:")
-    # Initialise a dictionary for each target's metrics.
+    # Initialize a dictionary for each target's metrics.
     avg_metrics: dict[str, dict] = {
-        target: {'R2': [], 'MSE': [], 'MAE': []} for target in targets
+        target: {'R2': [], 'MSE': [], 'RMSE': [], 'MAE': []} for target in targets
     }
     
     # Set the values into the dictionary.
@@ -49,6 +23,7 @@ def print_average_metrics(
         for target in targets:
             avg_metrics[target]['R2'].append(fold_metrics[target]['R2'])
             avg_metrics[target]['MSE'].append(fold_metrics[target]['MSE'])
+            avg_metrics[target]['RMSE'].append(fold_metrics[target]['RMSE'])
             avg_metrics[target]['MAE'].append(fold_metrics[target]['MAE'])
 
     # Print the values from the dictionary.
@@ -56,6 +31,7 @@ def print_average_metrics(
         print(f"\nAverage metrics for {target}:")
         print(f"R²: {npmean(avg_metrics[target]['R2']):.4f} (±{npstd(avg_metrics[target]['R2']):.4f})")
         print(f"MSE: {npmean(avg_metrics[target]['MSE']):.2f} (±{npstd(avg_metrics[target]['MSE']):.2f})")
+        print(f"RMSE: {npmean(avg_metrics[target]['RMSE']):.2f} (±{npstd(avg_metrics[target]['RMSE']):.2f})")
         print(f"MAE: {npmean(avg_metrics[target]['MAE']):.2f} (±{npstd(avg_metrics[target]['MAE']):.2f})")
 
 
@@ -69,27 +45,23 @@ def print_feature_importances(model: Booster, features: list[str]) -> None:
         print("\nNo feature importances available.")
         return
     total_splits: int = sum(importances.values())
-    if total_splits == 0:
-        print("\nTotal splits is zero, no importances to compute.")
-        return
-    print("\nFeature importances:")
-    for i, feat in enumerate(features):
-        imp: float = importances.get(f"f{i}", 0) / total_splits
-        print(f"{feat}: {imp:.4f}")
+    print("\nFeature importances:\n")
+    for feature in features:
+        importance: float = importances.get(feature, 0) / total_splits
+        print(f"{feature}: {importance:.4f}")
 
 
 def print_prediction_metrics(
-    y_test: ndarray,
-    y_pred: ndarray,
+    y_test: DataFrame,
+    y_pred: DataFrame,
     model: Booster,
     targets: list[str] = TARGETS
 ) -> None:
     """Prints prediction metrics."""
-    # Print prediction metrics.
     for i, target in enumerate(targets):
         print(f"\nMetrics for {target}:")
         print(f"R²: {r2_score(y_test[:, i], y_pred[:, i]):.4f}")
-        print(f"MSE: {mean_squared_error(y_test[:, i], y_pred[:, i]):.2f}")
-        print(f"MAE: {mean_absolute_error(y_test[:, i], y_pred[:, i]):.2f}")
+        print(f"MSE: {mean_squared_error(y_test[target], y_pred[target]):.2f}")
+        print(f"MAE: {mean_absolute_error(y_test[target], y_pred[target]):.2f}")
     print(f"Best Iteration: {model.best_iteration}")
-    print(f"Validation RMSE at best iteration: {model.best_score:.2f}")
+    print(f"Test RMSE at best iteration: {model.best_score:.2f}")
