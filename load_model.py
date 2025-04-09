b8943f7b0dc69c29954b6c8485c7026af2fb48e8
jvkloc <120744120+jvkloc@users.noreply.github.com>
2025-04-09 10:34:58 +0300

    Update load_model.py

--- load_model.py
+++ load_model.py
@@ -7,20 +7,15 @@ from xgboost import Booster, DMatrix
 from constants import MODEL_PATH
 
 
-def load_model(path: str = MODEL_PATH) -> Booster:
+def load_xgb_model(path: str = MODEL_PATH) -> Booster:
     """Returns a saved XGBoost model from the path."""
     model = Booster()
     model.load_model(path)
     return model
 
 
-def predict_with_loaded_model(
-    model: Booster, X_test: DataFrame, y_test: DataFrame
-) -> DataFrame:
-    """Predict with best iteration of the given model."""
-    best_iteration: int = model.best_iteration
-    dtest = DMatrix(X_test, label=y_test)
-    irange: tuple[int, int] = (0, best_iteration + 1)
-    y_pred: ndarray = model.predict(dtest, iteration_range=irange)
-    print(f"Loaded model best iteration: {best_iteration}")
+def predict_with_loaded_model(model: Booster, X_test: DataFrame) -> ndarray:
+    """Returns a prediction by the given model."""
+    dtest = DMatrix(X_test)
+    y_pred: ndarray = model.predict(dtest)
     return y_pred
