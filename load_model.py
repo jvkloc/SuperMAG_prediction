11db00942f41f1376a9a67ce768fe22608a30ff4
jvkloc <120744120+jvkloc@users.noreply.github.com>
2025-05-22 23:17:30 +0300

    Update load_model.py

--- load_model.py
+++ load_model.py
@@ -1,21 +1,29 @@
 """Functions for using a saved model."""
 
 from numpy import ndarray
-from pandas import DataFrame
+from polars import DataFrame as PolarsFrame
 from xgboost import Booster, DMatrix
 
-from constants import MODEL_PATH
 
-
-def load_xgb_model(path: str = MODEL_PATH) -> Booster:
+def load_xgb_model(path: str) -> Booster:
     """Returns a saved XGBoost model from the path."""
     model = Booster()
     model.load_model(path)
     return model
 
 
-def predict_with_loaded_model(model: Booster, X_test: DataFrame) -> ndarray:
-    """Returns a prediction by the given model."""
-    dtest = DMatrix(X_test)
+def predict_with_loaded_model(
+    model: Booster,
+    X_test: PolarsFrame,
+    y_test: PolarsFrame
+) -> ndarray:
+    """Returns a prediction by the given model based on the X_test data."""
+    Xtest: PolarsFrame = X_test.drop("index")
+    ytest: PolarsFrame = y_test.drop("index")
+    dtest = DMatrix(
+        data=Xtest.to_numpy(), 
+        label=ytest.to_numpy().ravel(), 
+        feature_names=Xtest.columns
+    )
     y_pred: ndarray = model.predict(dtest)
     return y_pred
