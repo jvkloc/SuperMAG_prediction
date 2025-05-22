e5fb195d692b15aee1052809ef1153e205c2d387
jvkloc <120744120+jvkloc@users.noreply.github.com>
2025-05-22 23:20:07 +0300

    Update plotting.py

--- plotting.py
+++ plotting.py
@@ -3,48 +3,45 @@
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 from numpy import ndarray
-from pandas import DataFrame
+from polars import DataFrame as PolarsFrame
 from scipy.stats import probplot
 
 from constants import FEATURES, TARGETS
 
 
-def plot_features_time_series(X_test: DataFrame, features: dict = FEATURES) -> None:
+def plot_features_time_series(X_test: PolarsFrame, features: dict = FEATURES) -> None:
     """Plots feature time series in three figures, each with three subplots."""
-    for group_name, features in features.items():
-        fig: ndarray; ax: Figure
+    time: ndarray = X_test["index"].to_numpy()
+
+    for group_name, group_features in features.items():
         fig, ax = plt.subplots(3, 1, figsize=(12, 10), sharex=True)
         axes: ndarray = ax.flatten()
         
-        # Colors for features.
-        colors = ["blue", "orange", "green"]
-        
-        # Plot each feature in its subplot.
-        for i, (feature, color) in enumerate(zip(features, colors)):
-            ax: ndarray = axes[i]
-            ax.plot(X_test.index, X_test[feature], color=color, label=feature, alpha=0.7)
-            ax.set_ylabel(f"{feature} value")
-            ax.set_title(f"{feature}")
-            ax.legend()
-            ax.grid(True)
-        
-        # Set x-label only for bottom subplot.
+        colors: list[str] = ["blue", "orange", "green"]
+
+        for i, (feature, color) in enumerate(zip(group_features, colors)):
+            values: ndarray = X_test[feature].to_numpy()
+            
+            axes[i].plot(time, values, color=color, label=feature, alpha=0.7)
+            axes[i].set_ylabel(f"{feature} value")
+            axes[i].set_title(f"{feature}")
+            axes[i].legend()
+            axes[i].grid(True)
+
         axes[2].set_xlabel("Time")
-        # Title above all subplots
         fig.suptitle(f"{group_name} time series", y=1.02)
-        # Display.
         plt.tight_layout()
         plt.show()
 
 
-def plot_evals_result(evals_result: dict, best_iteration: int) -> None:
-    """Plot training and test RMSE as a scatterplot."""
-    train_rmse: list[float] = evals_result['train']['rmse']
-    test_rmse: list[float] = evals_result['test']['rmse']
+def plot_evals_result(evals_result: dict, metric: str, best_iteration: int) -> None:
+    """Plot train and validation evaluation metric as a scatterplot."""
+    train_rmse: list[float] = evals_result['train'][metric]
+    test_rmse: list[float] = evals_result['test'][metric]
     iterations = range(len(train_rmse))  # X-axis: iteration numbers
     plt.figure(figsize=(10, 6))
-    plt.scatter(iterations, train_rmse, label="Training RMSE", color="blue")
-    plt.scatter(iterations, test_rmse, label="Test RMSE", color="orange")
+    plt.scatter(iterations, train_rmse, label=f"Training {metric}", color="blue")
+    plt.scatter(iterations, test_rmse, label=f"Validation {metric}", color="orange")
     # Set vertical line to best iteration number on x-axis.
     plt.axvline(
         x=best_iteration,
@@ -54,9 +51,9 @@ def plot_evals_result(evals_result: dict, best_iteration: int) -> None:
     )
     # Set axes' labels. 
     plt.xlabel("Iteration")
-    plt.ylabel("RMSE")
+    plt.ylabel(metric.upper())
     # Set plot title and legend.
-    plt.title("Training vs. test RMSE")
+    plt.title(f"Training vs. validation {metric.upper()}")
     plt.legend()
     # Plot image.
     plt.grid(True)
@@ -64,8 +61,8 @@ def plot_evals_result(evals_result: dict, best_iteration: int) -> None:
 
 
 def prediction_scatter_plot(
-    y_pred: DataFrame,
-    y_test: DataFrame,
+    y_pred: PolarsFrame,
+    y_test: PolarsFrame,
     targets: list[str] = TARGETS
 ) -> None:
     """Plots prediction vs. true values as a scatter plot."""
@@ -94,8 +91,8 @@ def prediction_scatter_plot(
 
 
 def global_prediction_scatter_plot(
-    y_pred: DataFrame,
-    y_test: DataFrame,
+    y_pred: PolarsFrame,
+    y_test: PolarsFrame,
     target: str = "SMR",
 ) -> None:
     """Plots prediction vs. true values as a scatter plot."""
@@ -120,30 +117,30 @@ def global_prediction_scatter_plot(
 
 
 def prediction_time_series(
-    y_pred: DataFrame,
-    y_test: DataFrame,
+    y_pred: PolarsFrame,
+    y_test: PolarsFrame,
     targets: list[str] = TARGETS,
 ) -> None:
-    """Plots prediction vs. true values as time series."""
-    _: ndarray; axs: Figure
+    """Plots local SMR predictions vs. true values as time series."""
     _, axs = plt.subplots(2, 2, figsize=(14, 10), sharex=True)
     axes: ndarray = axs.flatten()
-    # Colors for true (solid) and predicted (dashed)
+
     true_color: str = "blue"
     pred_color: str = "orange"
-    # Plot each target in its subplot.
+    time: ndarray = y_test["index"].to_numpy()
+
     for i, target in enumerate(targets[1:]):
-        ax: ndarray = axes[i]
+        ax: int = axes[i]
         ax.plot(
-            y_test.index,
-            y_test[target],
+            time,
+            y_test[target].to_numpy(),
             color=true_color,
             label=f"True {target}",
             alpha=0.7,
         )
         ax.plot(
-            y_test.index,
-            y_pred[target],
+            time,
+            y_pred[target].to_numpy(),
             color=pred_color,
             linestyle="--",
             label=f"Pred {target}",
@@ -153,54 +150,44 @@ def prediction_time_series(
         ax.set_title(f"{target}: True vs. predicted")
         ax.legend()
         ax.grid(True)
-    # Set x-label only for bottom row.
+
     axes[2].set_xlabel("Time")
     axes[3].set_xlabel("Time")
-    # Adjust layout and display,
+
     plt.tight_layout()
     plt.show()
 
 
 def global_prediction_time_series(
-    y_pred: DataFrame,
-    y_test: DataFrame,
+    y_pred: PolarsFrame,
+    y_test: PolarsFrame,
     target: str = "SMR",
 ) -> None:
-    """Plots prediction vs. true values as time series."""
+    """Plots global SMR prediction vs. true values as time series."""
     plt.figure(figsize=(10, 6))
-    # Colors for true (solid) and predicted (dashed).
-    true_color: str = "blue"
-    pred_color: str = "orange"
-    # Plot the target.
-    plt.plot(
-        y_test.index,
-        y_test[target],
-        color=true_color,
-        label=f"True {target}",
-        alpha=0.7,
-    )
-    plt.plot(
-        y_test.index,
-        y_pred[target],
-        color=pred_color,
-        linestyle="--",
-        label=f"Pred {target}",
-        alpha=0.7,
-    )
-    # Set labels and title.
+    
+    # Extract time axis and values
+    time: ndarray = y_test["index"].to_numpy()
+    true_vals: ndarray = y_test[target].to_numpy()
+    pred_vals: ndarray = y_pred[target].to_numpy()
+
+    # Plot
+    plt.plot(time, true_vals, color="blue", label=f"True {target}", alpha=0.7)
+    plt.plot(time, pred_vals, color="orange", linestyle="--", label=f"Pred {target}", alpha=0.7)
+
     plt.xlabel("Time")
     plt.ylabel(f"{target} (nT)")
     plt.title(f"{target}: True vs. predicted")
-    # Add legend and grid.
     plt.legend()
     plt.grid(True)
-    # Adjust layout and display,
     plt.tight_layout()
     plt.show()
 
 
-def plot_smr_histograms(data: DataFrame, targets: list[str] = TARGETS) -> None:
-    """Plots density histograms of SMR and SMR MLT values from all data."""
+def plot_smr_histograms(
+    data: PolarsFrame, label: str, targets: list[str] = TARGETS
+) -> None:
+    """Plots density histograms of SMR and SMR MLT values from the data."""
     _: ndarray; axes: Figure
     _, axes = plt.subplots(3, 2, figsize=(12, 10))
     axes: ndarray = axes.flatten()
@@ -213,7 +200,7 @@ def plot_smr_histograms(data: DataFrame, targets: list[str] = TARGETS) -> None:
         axes[i].hist(data[target], bins=50, color=color, alpha=0.7, density=True)
         axes[i].set_title(f"{target}")
         axes[i].set_xlabel(f"{target} (nT)")
-        axes[i].set_ylabel("Density")
+        axes[i].set_ylabel(f"{label} data SMR densities")
         axes[i].grid(True)
     
     # Hide the unused subplot (bottom right).
@@ -223,31 +210,36 @@ def plot_smr_histograms(data: DataFrame, targets: list[str] = TARGETS) -> None:
     plt.show()
 
 
-def residual_density_histogram(residuals: DataFrame) -> None:
+def residual_density_histogram(residuals: PolarsFrame) -> None:
     """Plots residuals in a normalized histogram."""
-    residuals.hist(bins=100, edgecolor="black", figsize=(10, 6), density=True)
+    values: ndarray = residuals.select(residuals.columns[0]).to_series().to_numpy()
+    plt.hist(values, bins=100, edgecolor="black", density=True)
     plt.title("Residual histogram")
     plt.xlabel("Residual")
     plt.ylabel("Frequency")
+    plt.tight_layout()
     plt.show()
 
 
-def residuals_vs_predicted_scatter(
-        residuals: DataFrame, y_pred: DataFrame
-) -> None:
+def residuals_vs_predicted_scatter(residuals: PolarsFrame, y_pred: PolarsFrame) -> None:
     """Scatter plot of residuals vs. predicted values. Residuals should be
     randomly scattered around zero line."""
     for col in residuals.columns:
+        # Convert to NumPy arrays.
+        y: ndarray = y_pred[col].to_numpy()
+        r: ndarray = residuals[col].to_numpy()
+        # Plot.
         plt.figure(figsize=(10, 6))
-        plt.scatter(y_pred[col], residuals[col], alpha=0.5)
+        plt.scatter(y, r, alpha=0.5)
         plt.axhline(y=0, color='r', linestyle="--")
-        plt.title(f"Residuals vs. predicted {col}")
+        plt.title(f"Residuals vs. predicted: {col}")
         plt.xlabel("Predicted values")
         plt.ylabel("Residuals")
+        plt.tight_layout()
         plt.show()
 
 
-def q_q_plot(residuals: DataFrame) -> None:
+def q_q_plot(residuals: PolarsFrame) -> None:
     """Q-Q plot for checking normality of the residuals. Note that XGBoost 
     does not require normality."""
     for col in residuals.columns:
